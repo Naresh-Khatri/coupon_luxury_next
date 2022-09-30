6f46da30b66143dfcc6f29775f30f677ef456dd8
naresh-khatri <naresh.khatri2345@gmail.com>
2022-09-30 08:48:40 +0530

    this component doest fetch data from server but receives it as a prop

--- components/home/StoresCarousel.js
+++ components/home/StoresCarousel.js
@@ -1,4 +1,12 @@
-import { Box, Button, Center, Grid, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Grid,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import axios from "axios";
 import Image from "next/image";
@@ -6,16 +14,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import OfferCard from "../DealCard";
 
-function DealsOfTheDay() {
-  const [stores, setStores] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:4000/stores?limit=20").then((res) => {
-      console.log(res.data);
-      setStores(res.data);
-    });
-  }, []);
-
+function DealsOfTheDay({ featuredStores }) {
   const options = {
     type: "loop",
     autoplay: true,
@@ -39,7 +38,7 @@ function DealsOfTheDay() {
 
   return (
     <Box w="full">
-      <Center>
+      <Center p={10}>
         <Text as={"h2"} fontSize="5xl" textAlign={"center"}>
           More Than
           <Text
@@ -54,48 +53,52 @@ function DealsOfTheDay() {
         </Text>
       </Center>
       <Center>
-        <Text as={"p"} opacity={0.5}>
+        <Text as={"p"} opacity={0.5} mt={2}>
           Search your favourite store & get many deals
         </Text>
       </Center>
-      <Splide
-        aria-label="Offers"
-        options={options}
-        className="carousel-container"
-      >
-        {stores.map((store) => (
-          <SplideSlide key={store._id}>
-            <Box w={"130px"} h={"80px"}>
-              <Image
-                style={{ borderRadius: "5px", margin: "15px 0px" }}
-                width={100}
-                height={50}
-                src={store.image}
-                alt={store.name}
-              />
-            </Box>
-          </SplideSlide>
-        ))}
-      </Splide>
-      <Splide
-        aria-label="Offers"
-        options={{ ...options, direction: "rtl" }}
-        className="carousel-container"
-      >
-        {stores.map((store) => (
-          <SplideSlide key={store._id}>
-            <Box w={"130px"} h={"80px"}>
-              <Image
-                style={{ borderRadius: "5px" }}
-                width={"100"}
-                height={50}
-                src={store.image}
-                alt={store.name}
-              />
-            </Box>
-          </SplideSlide>
-        ))}
-      </Splide>
+      <Flex h={160} my={10} direction="column" justifyContent="space-between">
+        <Splide
+          aria-label="Offers"
+          options={options}
+          className="carousel-container"
+        >
+          {featuredStores.map((store) => (
+            <SplideSlide key={store._id}>
+              <Box w={"100px"} h={"50px"} mt={5}>
+                <Image
+                  style={{ borderRadius: "5px" }}
+                  width={100}
+                  height={50}
+                  sizes={"100%"}
+                  src={store.image}
+                  alt={store.name}
+                />
+              </Box>
+            </SplideSlide>
+          ))}
+        </Splide>
+        <Splide
+          aria-label="Offers"
+          options={{ ...options, direction: "rtl" }}
+          className="carousel-container"
+        >
+          {featuredStores.map((store) => (
+            <SplideSlide key={store._id}>
+              <Box w={"100px"} h={"50px"} mt={5}>
+                <Image
+                  style={{ borderRadius: "5px" }}
+                  sizes={"100%"}
+                  width={100}
+                  height={50}
+                  src={store.image}
+                  alt={store.name}
+                />
+              </Box>
+            </SplideSlide>
+          ))}
+        </Splide>
+      </Flex>
     </Box>
   );
 }
