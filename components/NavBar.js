e60ee6f872759332ff4618eb70186cd06ef49231
naresh-khatri <naresh.khatri2345@gmail.com>
2022-09-30 08:45:03 +0530

    add logic to know active route and enable routing

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,7 +4,6 @@ import {
   Flex,
   Avatar,
   HStack,
-  Link,
   IconButton,
   Button,
   Menu,
@@ -29,49 +28,59 @@ import {
   SearchIcon,
 } from "@chakra-ui/icons";
 import Image from "next/image";
-
-import useWidth from "../hooks/useWidth";
+import Link from "next/link";
+import { Link as CLink } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 const Links = [
-  { name: "Home", display: "900" },
-  { name: "Stores", expanable: true, display: "100", active: true },
-  { name: "Categories", expanable: true, display: "750" },
-  { name: "Blogs", display: "1000" },
+  { name: "Home", display: "900", slug: "/" },
+  {
+    name: "Stores",
+    expanable: true,
+    display: "100",
+    slug: "/stores",
+  },
+  { name: "Categories", expanable: true, display: "750", slug: "/categories" },
+  { name: "Blogs", display: "1000", slug: "/blogs" },
 ];
 
-const NavLink = ({ children, active = undefined }) => (
-  <Link
-    fontSize={18}
-    p={2}
-    rounded={"md"}
-    _hover={{
-      textDecoration: "none",
-      bg: "brand.1000",
-    }}
-    bg={active ? "brand.1000" : undefined}
-    href={"#"}
-  >
-    {children}
-  </Link>
-);
+function NavLink({ children, slug }) {
+  const router = useRouter();
+  return (
+    <Link href={slug} passHref>
+      <CLink
+        fontSize={18}
+        p={2}
+        rounded={"md"}
+        _hover={{
+          textDecoration: "none",
+          bg: "brand.1000",
+        }}
+        bg={router.pathname == slug ? "brand.1000" : undefined}
+      >
+        <Text>{children}</Text>
+      </CLink>
+    </Link>
+  );
+}
 
 function NavBar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const width = useWidth();
   return (
     <>
-      <Box bg="brand.900" color="white" alignItems={"center"} px={4} h={75} position='fixed'>
+      <Box bg="brand.900" color="white" alignItems={"center"} px={4} h={75}>
         <Flex h="100%" alignItems={"center"} justifyContent={"space-between"}>
           <HStack spacing={8}>
             <Link href="https://www.couponluxury.com/">
-              <Image
-                title="Home"
-                src="https://ik.imagekit.io/couponluxury/main_logo_noj4ZyPyq"
-                alt="logo"
-                width="105"
-                height="50"
-                loading="lazy"
-              />
+              <a>
+                <Image
+                  title="Home"
+                  src="https://ik.imagekit.io/couponluxury/main_logo_noj4ZyPyq"
+                  alt="logo"
+                  width="105"
+                  height="50"
+                />
+              </a>
             </Link>
             <HStack
               as={"nav"}
@@ -80,7 +89,7 @@ function NavBar() {
               alignItems="center"
             >
               {Links.map((link) => (
-                <NavLink key={link.name} active={link.active}>
+                <NavLink key={link.name} slug={link.slug}>
                   {link.name}
                 </NavLink>
               ))}
@@ -98,37 +107,6 @@ function NavBar() {
                 <SearchIcon color="blackAlpha.600" />
               </InputRightElement>
             </InputGroup>
-            {/* <Button
-              variant={"solid"}
-              colorScheme={"teal"}
-              size={"sm"}
-              mr={4}
-              leftIcon={<AddIcon />}
-            >
-              Action
-            </Button>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={"full"}
-                variant={"link"}
-                cursor={"pointer"}
-                minW={0}
-              >
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu> */}
           </Flex>
 
           <IconButton
@@ -167,15 +145,18 @@ function NavBar() {
                     flexDir="column"
                     alignItems="flex-end"
                     py={100}
+                    pr={8}
                   >
                     {Links.map((link) => (
-                      <Text
-                        fontSize="40"
-                        fontWeight="extrabold"
-                        key={link.name}
-                      >
-                        {link.name}
-                      </Text>
+                      <Box key={link.name} onClick={onClose}>
+                        <Link href={link.slug}>
+                          <a>
+                            <Text fontSize="40" fontWeight="extrabold">
+                              {link.name}
+                            </Text>
+                          </a>
+                        </Link>
+                      </Box>
                     ))}
                   </Stack>
                 </Box>
