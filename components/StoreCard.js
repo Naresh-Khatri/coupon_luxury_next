431067e0bb5058a9fd36b750bb86bf939909112d
naresh-khatri <naresh.khatri2345@gmail.com>
2022-09-30 08:50:08 +0530

    wrap store card component in a <Link> tag

--- components/StoreCard.js
+++ components/StoreCard.js
@@ -1,24 +1,22 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Link, Text } from "@chakra-ui/react";
 import Image from "next/image";
 
 function StoreCard({ title, slug, img }) {
   return (
-    <Box
-      width={{ base: 135, lg: 180 }}
-      height={{ base: 65, lg: 140 }}
-      shadow="2xl"
-      borderRadius={10}
-      bg="white"
-    >
-      <Image
-        src={img}
-        width={135}
-        height={65}
-        alt={`${title} - logo`}
-        style={{ width: "100%" }}
-      />
-      <Text textAlign={"center"}>{title}</Text>
-    </Box>
+    <Link href={`/stores/${slug}`}>
+        <Box shadow="2xl" borderRadius={10} bg="white">
+          <Image
+            src={img}
+            width={135}
+            height={65}
+            alt={`${title} - logo`}
+            sizes="100%"
+          />
+          <Text py={2} textAlign={"center"}>
+            {title}
+          </Text>
+        </Box>
+    </Link>
   );
 }
 
