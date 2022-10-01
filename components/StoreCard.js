9f54d769d004f1920b38c8635514caaf7ff3f094
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-01 20:40:09 +0530

    set number of lines for storeName to 2

--- components/StoreCard.js
+++ components/StoreCard.js
@@ -4,18 +4,19 @@ import Image from "next/image";
 function StoreCard({ title, slug, img }) {
   return (
     <Link href={`/stores/${slug}`}>
-        <Box shadow="2xl" borderRadius={10} bg="white">
-          <Image
-            src={img}
-            width={135}
-            height={65}
-            alt={`${title} - logo`}
-            sizes="100%"
-          />
-          <Text py={2} textAlign={"center"}>
-            {title}
-          </Text>
-        </Box>
+      <Box shadow="2xl" borderRadius={10} bg="white">
+        <Image
+          src={img}
+          width={135}
+          height={65}
+          alt={`${title} - logo`}
+          sizes="100%"
+          style={{ borderRadius: "15px" }}
+        />
+        <Text py={2} textAlign={"center"} noOfLines={1}>
+          {title}
+        </Text>
+      </Box>
     </Link>
   );
 }
