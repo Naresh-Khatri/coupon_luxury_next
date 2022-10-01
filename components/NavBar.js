61088cfe4043b83c634a4fcbb44b46ca9c3bd445
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-01 20:39:42 +0530

    create dropdowns for store and category and new component for search bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -26,11 +26,15 @@ import {
   CloseIcon,
   AddIcon,
   SearchIcon,
+  ChevronDownIcon,
 } from "@chakra-ui/icons";
 import Image from "next/image";
 import Link from "next/link";
 import { Link as CLink } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import SearchBox from "./SearchBox";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Links = [
   { name: "Home", display: "900", slug: "/" },
@@ -65,107 +69,213 @@ function NavLink({ children, slug }) {
 }
 
 function NavBar() {
+  const router = useRouter();
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [featuredStores, setFeaturedStores] = useState([]);
+  const [featuredCategories, setFeaturedCategories] = useState([]);
+
+  useEffect(() => {
+    const promises = [
+      axios.get("http://localhost:4000/stores?featered=true&limit=10"),
+      axios.get("http://localhost:4000/categories?featered=true&limit=10"),
+    ];
+    Promise.allSettled(promises).then((res) => {
+      setFeaturedStores(res[0].value.data);
+      setFeaturedCategories(res[1].value.data);
+      // console.log(featuredStores, featuredCategories);
+    });
+  }, []);
   return (
     <>
-      <Box bg="brand.900" color="white" alignItems={"center"} px={4} h={75}>
-        <Flex h="100%" alignItems={"center"} justifyContent={"space-between"}>
-          <HStack spacing={8}>
-            <Link href="https://www.couponluxury.com/">
-              <a>
-                <Image
-                  title="Home"
-                  src="https://ik.imagekit.io/couponluxury/main_logo_noj4ZyPyq"
-                  alt="logo"
-                  width="105"
-                  height="50"
-                />
-              </a>
-            </Link>
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-              alignItems="center"
-            >
-              {Links.map((link) => (
-                <NavLink key={link.name} slug={link.slug}>
-                  {link.name}
-                </NavLink>
-              ))}
-            </HStack>
-          </HStack>
-          <Flex alignItems={"center"} flexGrow={1} maxW={350}>
-            <InputGroup size="sm">
-              <Input
-                h={"40px"}
-                borderRadius={10}
-                placeholder="Search"
-                bg="white"
-              />
-              <InputRightElement h={"40px"}>
-                <SearchIcon color="blackAlpha.600" />
-              </InputRightElement>
-            </InputGroup>
-          </Flex>
-
-          <IconButton
-            zIndex={101}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            bg={"transparent !important"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-        </Flex>
-        <Slide direction="top" in={isOpen} style={{ zIndex: 10 }}>
-          <Fade in={isOpen}>
-            <Box
-              p="40px"
-              color="white"
-              mt="4"
-              bg="teal.500"
-              rounded="md"
-              shadow="md"
-            >
-              {isOpen && (
-                <Box
-                  position="absolute"
-                  top={0}
-                  left={0}
-                  zIndex={100}
-                  w="100vw"
-                  h="100vh"
-                  display={{ md: "none" }}
-                  className="ham-background"
-                >
-                  <Stack
-                    as={"nav"}
-                    display="flex"
-                    flexDir="column"
-                    alignItems="flex-end"
-                    py={100}
-                    pr={8}
+      <Flex bg="brand.900" justify={"center"}>
+        <Box color="white" px={4} h={75} maxW={1200} w={"100vw"}>
+          <Flex h="100%" alignItems={"center"} justifyContent={"space-between"}>
+            <HStack spacing={8}>
+              <Link href="https://www.couponluxury.com/">
+                <a>
+                  <Image
+                    title="Home"
+                    src="https://ik.imagekit.io/couponluxury/main_logo_noj4ZyPyq"
+                    alt="logo"
+                    width="120"
+                    height="60"
+                  />
+                </a>
+              </Link>
+              <HStack
+                as={"nav"}
+                display={{ base: "none", md: "flex" }}
+                alignItems="center"
+              >
+                {/* {Links.map((link) => (
+                  <NavLink key={link.name} slug={link.slug}>
+                    {link.name}
+                  </NavLink>
+                ))} */}
+                <NavLink slug={"/"}>{"Home"}</NavLink>
+                <Menu>
+                  <MenuButton
+                    p={2}
+                    borderRadius={"md"}
+                    bg={
+                      router.pathname.includes("/stores")
+                        ? "brand.1000"
+                        : undefined
+                    }
                   >
-                    {Links.map((link) => (
-                      <Box key={link.name} onClick={onClose}>
-                        <Link href={link.slug}>
+                    <Text fontSize={18}>
+                      Stores
+                      <ChevronDownIcon color={"white"} />
+                    </Text>
+                  </MenuButton>
+
+                  <MenuList color={"black"} minWidth="10%">
+                    <MenuItem>
+                      <Link href={`/stores`}>
+                        <a>
+                          <Text fontWeight={"extrabold"}>All Stores</Text>
+                        </a>
+                      </Link>
+                    </MenuItem>
+                    {featuredStores.map((store) => (
+                      <MenuItem key={store._id}>
+                        <Link href={`/stores/${store.slug}`}>
                           <a>
-                            <Text fontSize="40" fontWeight="extrabold">
-                              {link.name}
-                            </Text>
+                            <Image
+                              src={store.image}
+                              alt={store.name}
+                              width={80}
+                              height={40}
+                            />
                           </a>
                         </Link>
-                      </Box>
+                      </MenuItem>
                     ))}
-                  </Stack>
-                </Box>
-              )}
-            </Box>
-          </Fade>
-        </Slide>
-      </Box>
+                    {/* <MenuItem>Create a Copy</MenuItem>
+                    <MenuItem>Mark as Draft</MenuItem>
+                    <MenuItem>Delete</MenuItem>
+                    <MenuItem>Attend a Workshop</MenuItem> */}
+                  </MenuList>
+                </Menu>
+                <Menu>
+                  <MenuButton
+                    p={2}
+                    borderRadius={"md"}
+                    bg={
+                      router.pathname.includes("/categories")
+                        ? "brand.1000"
+                        : undefined
+                    }
+                  >
+                    <Text fontSize={18}>
+                      Categories
+                      <ChevronDownIcon color={"white"} />
+                    </Text>
+                  </MenuButton>
+                  <MenuList color={"black"} minWidth="10%">
+                    <MenuItem>
+                      <Link href={`/categories`}>
+                        <a>
+                          <Text fontWeight={"extrabold"}>All Categories</Text>
+                        </a>
+                      </Link>
+                    </MenuItem>
+                    {featuredCategories.map((category) => (
+                      <MenuItem key={category._id}>
+                        <Link href={`/stores/${category.slug}`}>
+                          <a>{category.categoryName}</a>
+                        </Link>
+                      </MenuItem>
+                    ))}
+                    {/* <MenuItem>Create a Copy</MenuItem>
+                    <MenuItem>Mark as Draft</MenuItem>
+                    <MenuItem>Delete</MenuItem>
+                    <MenuItem>Attend a Workshop</MenuItem> */}
+                  </MenuList>
+                </Menu>
+                {/* <NavLink slug={link.slug}>{link.name}</NavLink>
+                <NavLink slug={link.slug}>{link.name}</NavLink> */}
+                <NavLink slug={"/blogs"}>{"Blogs"}</NavLink>
+              </HStack>
+            </HStack>
+            <SearchBox />
+
+            <IconButton
+              zIndex={101}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={"Open Menu"}
+              bg={"transparent !important"}
+              display={{ md: "none" }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+          </Flex>
+          <Slide direction="top" in={isOpen} style={{ zIndex: 10 }}>
+            <Fade in={isOpen}>
+              <Box
+                p="40px"
+                color="white"
+                mt="4"
+                bg="teal.500"
+                rounded="md"
+                shadow="md"
+              >
+                {isOpen && (
+                  <Box
+                    position="absolute"
+                    top={0}
+                    left={0}
+                    zIndex={100}
+                    w="100vw"
+                    h="100vh"
+                    display={{ md: "none" }}
+                    className="ham-background"
+                  >
+                    <Stack
+                      as={"nav"}
+                      display="flex"
+                      flexDir="column"
+                      alignItems="flex-end"
+                      py={100}
+                      pr={8}
+                    >
+                      {Links.map((link) => (
+                        <Box key={link.name} onClick={onClose}>
+                          <Link href={link.slug}>
+                            <a>
+                              <Text fontSize="40" fontWeight="extrabold">
+                                {link.name}
+                              </Text>
+                            </a>
+                          </Link>
+                        </Box>
+                      ))}
+                    </Stack>
+                  </Box>
+                )}
+              </Box>
+            </Fade>
+          </Slide>
+        </Box>
+      </Flex>
     </>
   );
 }
+
 export default NavBar;
+
+// export const getServerSideProps = async () => {
+//   let res = await fetch("http://localhost:4000/stores?featered=true&limit=5");
+//   const navStores = await res.json();
+
+//   res = await fetch("http://localhost:4000/categories?featered=true&limit=5");
+//   const navCategories = await res.json();
+//   res = await fetch("http://localhost:4000/blogs");
+//   const blogs = await res.json();
+
+//   console.log(navStores, navCategories, res);
+
+//   return {
+//     props: { navStores, navCategories },
+//   };
+// };
