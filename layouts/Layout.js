08e637ab3f8b37e76f6a6659a0302588a2de114f
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-01 20:40:33 +0530

    remove unused code

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,8 +1,5 @@
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 import React from "react";
-import { Box } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
