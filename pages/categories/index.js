fb7eba946003e94d43a374a3ea985c3d16580dee
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-04 12:07:44 +0530

    add meta tags and replace getServerSideProps with getStaticProps with trycatch block

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -9,114 +9,136 @@ import {
   SimpleGrid,
   Text,
 } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faCube } from "@fortawesome/free-solid-svg-icons";
+import Banner from "../../components/Banner";
+
+import SetMeta from "../../utils/SetMeta";
 
 function index({ featuredCat }) {
   return (
-    <Flex bg={"#e0e0e0"} justifyContent="center">
-      <Center flexDirection={"column"} maxW={1240} px={2}>
-        <Box my={4} alignSelf={"flex-start"}>
-          <Breadcrumb
-            spacing="8px"
-            separator={<ChevronRightIcon color="gray.500" />}
-            color="brand.900"
-          >
-            <BreadcrumbItem>
-              <Link href="/" passHref>
-                <a>
-                  <Box fontSize="sm" _hover={{ color: "brand.1000" }}>
+    <Box>
+      <SetMeta
+        title={`All Deals and discount Offers in different categories`}
+        description={`Shop on all categories using the latest luxury coupon codes and discount offers and grab the best deals on your order. Extra 20% off on different categories`}
+        url={"https://www.couponluxury.com/categories"}
+      />
+      <Text as={"h1"} hidden>
+        All Deals and discount Offers in different categories
+      </Text>
+      <Banner
+        title={"All Categories"}
+        subTitle={`${featuredCat.length} Categories available`}
+      />
+      <Flex bg={"#e0e0e0"} justifyContent="center">
+        <Center flexDirection={"column"} maxW={1240} px={2}>
+          <Box my={4} alignSelf={"flex-start"}>
+            <Breadcrumb
+              spacing="8px"
+              separator={<ChevronRightIcon color="gray.500" />}
+              color="brand.900"
+            >
+              <BreadcrumbItem>
+                <Link href="/" passHref>
+                  <a>
+                    <Box fontSize="sm" _hover={{ color: "brand.1000" }}>
+                      <FontAwesomeIcon
+                        icon={faHouse}
+                        style={{ paddingRight: "10px" }}
+                      />
+                      Home
+                    </Box>
+                  </a>
+                </Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href="/" fontSize="sm">
+                  <Box>
                     <FontAwesomeIcon
-                      icon={faHouse}
+                      icon={faCube}
                       style={{ paddingRight: "10px" }}
                     />
-                    Home
+                    Categories
+                  </Box>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
+          </Box>
+          <SimpleGrid
+            columns={{ base: 1, md: 2, lg: 3 }}
+            spacing={5}
+            maxW={"1240px"}
+          >
+            {featuredCat.map((category) => (
+              <Link key={category._id} href={`/categories/${category.slug}`}>
+                <a>
+                  <Box
+                    position={"relative"}
+                    _hover={{
+                      opacity: 0.9,
+                      transform: "scale(1.07)",
+                      transition: "all .1s easy-in-out",
+                    }}
+                    style={{ transition: "all .1s ease-in-out" }}
+                  >
+                    <Image
+                      src={category.image}
+                      width={350}
+                      height={200}
+                      alt={category.imgAlt}
+                      style={{
+                        borderRadius: "15px",
+                        filter: "brightness(0.5)",
+                      }}
+                    />
+                    <Flex
+                      style={{ position: "absolute", top: 0, left: 0 }}
+                      w="350px"
+                      h={"200px"}
+                      direction="column"
+                      justify={"center"}
+                      align={"center"}
+                    >
+                      <Text
+                        as={"h4"}
+                        alignContent="center"
+                        color="white"
+                        fontSize={"3xl"}
+                        fontWeight={"extrabold"}
+                        textAlign={"center"}
+                      >
+                        {category.categoryName}
+                      </Text>
+                      <Text as={"p"} alignContent="center" color="white">
+                        {`${category.offers.length} Deals / Coupons`}
+                      </Text>
+                    </Flex>
                   </Box>
                 </a>
               </Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem isCurrentPage>
-              <BreadcrumbLink href="/" fontSize="sm">
-                <Box>
-                  <FontAwesomeIcon
-                    icon={faCube}
-                    style={{ paddingRight: "10px" }}
-                  />
-                  Categories
-                </Box>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-        </Box>
-        <SimpleGrid
-          columns={{ base: 1, md: 2, lg: 3 }}
-          spacing={5}
-          maxW={"1240px"}
-        >
-          {featuredCat.map((category) => (
-            <Link key={category._id} href={`/categories/${category.slug}`}>
-              <a>
-                <Box
-                  position={"relative"}
-                  _hover={{
-                    opacity: 0.9,
-                    transform: "scale(1.07)",
-                    transition: "all .1s easy-in-out",
-                  }}
-                  style={{ transition: "all .1s ease-in-out" }}
-                >
-                  <Image
-                    src={category.image}
-                    width={350}
-                    height={200}
-                    alt={category.imgAlt}
-                    style={{
-                      borderRadius: "15px",
-                      filter: "brightness(0.5)",
-                    }}
-                  />
-                  <Flex
-                    style={{ position: "absolute", top: 0, left: 0 }}
-                    w="350px"
-                    h={"200px"}
-                    direction="column"
-                    justify={"center"}
-                    align={"center"}
-                  >
-                    <Text
-                      as={"h4"}
-                      alignContent="center"
-                      color="white"
-                      fontSize={"3xl"}
-                      fontWeight={"extrabold"}
-                      textAlign={"center"}
-                    >
-                      {category.categoryName}
-                    </Text>
-                    <Text as={"p"} alignContent="center" color="white">
-                      {`${category.offers.length} Deals / Coupons`}
-                    </Text>
-                  </Flex>
-                </Box>
-              </a>
-            </Link>
-          ))}
-        </SimpleGrid>
-      </Center>
-    </Flex>
+            ))}
+          </SimpleGrid>
+        </Center>
+      </Flex>
+    </Box>
   );
 }
 
-export const getServerSideProps = async () => {
-  let res = await fetch("http://localhost:4000/categories");
-  const featuredCat = await res.json();
-  return {
-    props: {
-      featuredCat,
-    },
-  };
+export const getStaticProps = async () => {
+  try {
+    let res = await fetch("http://localhost:4000/categories");
+    const featuredCat = await res.json();
+    return {
+      props: {
+        featuredCat,
+      },
+      revalidate: 60,
+    };
+  } catch (err) {
+    return { redirect: { destination: "/not-found", permanent: false } };
+  }
 };
 
 export default index;
