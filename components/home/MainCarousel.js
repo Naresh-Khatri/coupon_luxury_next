0d02a91296be2d0ae6ec9aa60de4de62176a04e5
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-04 11:58:24 +0530

    import Image from 'next/futute/image' to about that edge slide render issue

--- components/home/MainCarousel.js
+++ components/home/MainCarousel.js
@@ -1,29 +1,29 @@
 import { Box } from "@chakra-ui/react";
-import Image from "next/image";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import styles from "../../styles/splide.module.css";
+import Image from "next/future/image";
 import Link from "next/link";
 
 function MainCarousel({ slides }) {
   const options = {
     type: "loop",
-    autoplay: true,
     interval: 3000,
+    autoplay: true,
     speed: 3000,
-    gap: "2rem",
     perMove: 1,
     rewind: true,
     easing: "cubic-bezier(0.25, 1, 0.5, 1)",
     perPage: 2,
     breakpoints: {
-      1100: { padding: "1rem" },
+      1100: {
+        //  padding: "1rem"
+      },
       600: {
         perPage: 1,
         arrows: false,
         width: "100%",
         pagination: false,
-        padding: "2rem",
       },
     },
   };
@@ -38,11 +38,15 @@ function MainCarousel({ slides }) {
         {slides.map((slide) => (
           <SplideSlide key={slide.imgURL}>
             <Link href={slide.link}>
-              <a target="_blank">
+              <a
+                target="_blank"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
                 <Image
+                  style={{ aspectRatio: "5/4" }}
                   className={styles.carousel__img}
                   width="610"
-                  height="450"
+                  height="500"
                   src={slide.imgURL}
                   alt={slide.imgAlt}
                 />
