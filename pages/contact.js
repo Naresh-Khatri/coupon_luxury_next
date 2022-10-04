c8425a731a09176104d66eb3499c639381446f5a
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-04 11:55:59 +0530

    add contact page

---
+++ pages/contact.js
@@ -0,0 +1,158 @@
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShop } from "@fortawesome/free-solid-svg-icons";
+import SetMeta from "../utils/SetMeta";
+import {
+  faFacebook,
+  faInstagram,
+  faPinterest,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+function ContactPage() {
+  return (
+    <>
+      <SetMeta
+        title="About - CouponLuxury"
+        url="https://www.couponluxury.com/about"
+      />
+      <Box bg="#ebf8ff" h={"100vh"} minH={"100vh"} w="full">
+        <Container
+          w={"full"}
+          maxW={1240}
+          h="full"
+          display={"flex"}
+          flexDirection={"column"}
+          alignContent={"center"}
+          justifyContent={"center"}
+        >
+          <Box mb={10} mt={-20}>
+            <Text
+              as={"h1"}
+              fontSize={{ base: "4xl", md: "5xl" }}
+              fontWeight={"extrabold"}
+              lineHeight={1}
+              py={3}
+              color="brand.900"
+            >
+              Contact Us
+            </Text>
+            <Box>
+              <Box color={"brand.900"} fontWeight="extrabold">
+                <Link href="mailto:contact@couponluxury.com">
+                  <a>contact@couponluxury.com</a>
+                </Link>
+              </Box>
+            </Box>
+            <HStack as={"nav"} spacing={7} mt={2}>
+              <Link href="https://facebook.com/CouponLuxury/">
+                <a target="_blank">
+                  <IconButton
+                    icon={
+                      <FontAwesomeIcon
+                        size="3x"
+                        icon={faFacebook}
+                        color="#4267B2"
+                      />
+                    }
+                  />
+                </a>
+              </Link>
+              <Link href="https://twitter.com/coupon_luxury">
+                <a target="_blank">
+                  <IconButton
+                    icon={
+                      <FontAwesomeIcon
+                        size="3x"
+                        icon={faTwitter}
+                        color="#1DA1F2"
+                      />
+                    }
+                  />
+                </a>
+              </Link>
+              <Link href="https://www.instagram.com/couponluxury/">
+                <a target="_blank">
+                  <IconButton
+                    icon={
+                      <FontAwesomeIcon
+                        size="3x"
+                        icon={faInstagram}
+                        color="#E1306C"
+                      />
+                    }
+                  />
+                </a>
+              </Link>
+              <Link href="https://youtube.com/channel/UCiGBpYZFIzyw_R5W1KUocJQ">
+                <a target="_blank">
+                  <IconButton
+                    icon={
+                      <FontAwesomeIcon
+                        size="3x"
+                        icon={faYoutube}
+                        color="#FF0000"
+                      />
+                    }
+                  />
+                </a>
+              </Link>
+              <Link href="https://www.pinterest.com/couponluxury/">
+                <a target="_blank">
+                  <IconButton
+                    icon={
+                      <FontAwesomeIcon
+                        size="3x"
+                        icon={faPinterest}
+                        color="#E60023"
+                      />
+                    }
+                  />
+                </a>
+              </Link>
+            </HStack>
+          </Box>
+          <Center>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+              <Flex w={"full"}>
+                <iframe
+                  id="map"
+                  src="https://maps.google.com/maps?q=gorgaon&output=embed"
+                  height="450"
+                  width={"100%"}
+                  loading="lazy"
+                  display={{ borderRadius: "10px" }}
+                ></iframe>
+              </Flex>{" "}
+              <Flex display={{ base: "none", md: "block" }}>
+                <Image
+                  src={"/assets/contact_us.svg"}
+                  alt="about couponluxury"
+                  width={500}
+                  height={500}
+                />
+              </Flex>
+            </SimpleGrid>
+          </Center>
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+export default ContactPage;
