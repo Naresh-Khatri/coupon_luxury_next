a7dbc5e24819c3f6cc297031a45089490aceec60
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-04 12:06:02 +0530

    create SetMeta component to accept and set meta tags

---
+++ utils/SetMeta.js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import React from "react";
+
+function SetMeta({ title, description, keywords, image, url }) {
+  return (
+    <Head>
+      {title && (
+        <>
+          <title>{title}</title>
+          <meta property="og:title" content={title} />
+          <meta name="twitter:title" content={title} />
+        </>
+      )}
+      {description && (
+        <>
+          <meta name="description" content={description} />
+          <meta property="og:description" content={description} />
+          <meta name="twitter:description" content={description} />
+        </>
+      )}
+      {keywords && (
+        <>
+          <meta name="keywords" content={keywords} />
+        </>
+      )}
+      {image && (
+        <>
+          <meta property="og:image" content={image} />
+          <meta name="twitter:image" content={image} />
+        </>
+      )}
+      {url && (
+        <>
+          <meta name="url" content={url} />
+          <meta property="og:url" content={url} />
+          <meta name="twitter:url" content={url} />
+          <link rel="canonical" href={url} />
+        </>
+      )}
+      {!url && <link rel="canonical" href="https://www.couponluxury.com" />}
+    </Head>
+  );
+}
+
+export default SetMeta;
