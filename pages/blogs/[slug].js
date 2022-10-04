9282c397e24f3bb60f18ddcd3efe65b69b55fe8b
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-04 12:06:28 +0530

    add meta tags and trycatch block

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -4,6 +4,8 @@ import React from "react";
 import BlogPreviewSmall from "../../components/BlogPreviewSmall";
 import styles from "./style.module.scss";
 
+import SetMeta from "../../utils/SetMeta";
+
 function BlogPage({ blogData, allBlogs }) {
   const {
     title,
@@ -16,70 +18,86 @@ function BlogPage({ blogData, allBlogs }) {
     formattedDate,
   } = blogData;
   return (
-    <Container maxW={"1200px"}>
-      <Grid
-        mt={10}
-        w="100%"
-        templateColumns={{ base: "1fr", lg: "repeat(3, 1fr)" }}
-      >
-        <GridItem colSpan={2}>
-          <Box p={5} shadow="2xl" borderRadius={15}>
-            <Text
-              as={"h1"}
-              fontSize={{ base: "4xl", md: "5xl" }}
-              fontWeight={"extrabold"}
-              lineHeight={1}
-              py={7}
-              color="brand.900"
-            >
-              {title}
+    <>
+      <SetMeta
+        title={`${blogData.metaTitle} - Coupons Luxury`}
+        description={blogData.metaDescription}
+        keywords={blogData.keywords}
+        url={"https://www.couponluxury.com/blogs/" + blogData.slug}
+      />
+      <Container maxW={"1200px"}>
+        <Grid
+          mt={10}
+          w="100%"
+          templateColumns={{ base: "1fr", lg: "repeat(3, 1fr)" }}
+        >
+          <GridItem colSpan={2}>
+            <Box p={5} shadow="2xl" borderRadius={15}>
+              <Text
+                as={"h1"}
+                fontSize={{ base: "4xl", md: "5xl" }}
+                fontWeight={"extrabold"}
+                lineHeight={1}
+                py={7}
+                color="brand.900"
+              >
+                {title}
+              </Text>
+              <Text as={"p"} pb={3}>
+                {formattedDate}
+              </Text>
+              <Image
+                src={coverImg}
+                sizes={"100%"}
+                alt={imgAlt}
+                width={768}
+                height={432}
+                style={{ borderRadius: "15px" }}
+              />
+              <Box
+                as="article"
+                mt={10}
+                dangerouslySetInnerHTML={{ __html: fullDescription }}
+                className={styles.page_html}
+              ></Box>
+            </Box>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <Text pl={5} fontSize={"4xl"}>
+              Latest Blogs
             </Text>
-            <Text as={"p"} pb={3}>
-              {formattedDate}
-            </Text>
-            <Image
-              src={coverImg}
-              sizes={"100%"}
-              alt={imgAlt}
-              width={768}
-              height={432}
-              style={{ borderRadius: "15px" }}
-            />
-            <Box
-              mt={10}
-              dangerouslySetInnerHTML={{ __html: fullDescription }}
-              className={styles.page_html}
-            ></Box>
-          </Box>
-        </GridItem>
-        <GridItem colSpan={1}>
-          <Text pl={5} fontSize={"4xl"}>
-            Latest Blogs
-          </Text>
-          <Box>
-            {allBlogs.map((blog) => {
-              return <BlogPreviewSmall key={blog._id} blog={blog} />;
-            })}
-          </Box>
-        </GridItem>
-      </Grid>
-    </Container>
+            <Box>
+              {allBlogs.map((blog) => {
+                return <BlogPreviewSmall key={blog._id} blog={blog} />;
+              })}
+            </Box>
+          </GridItem>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
 export const getServerSideProps = async (ctx) => {
-  let res = await fetch(
-    `http://localhost:4000/blogs/getUsingSlug/${ctx.query.slug}`
-  );
-  const blogData = await res.json();
-  res = await fetch("http://localhost:4000/blogs?limit=10");
-  const allBlogs = await res.json();
-  return {
-    props: {
-      blogData: { ...blogData, formattedDate: formatDate(blogData.createdAt) },
-      allBlogs,
-    },
-  };
+  try {
+    let res = await fetch(
+      `http://localhost:4000/blogs/getUsingSlug/${ctx.query.slug}`
+    );
+    const blogData = await res.json();
+    res = await fetch("http://localhost:4000/blogs?limit=10");
+    const allBlogs = await res.json();
+    return {
+      props: {
+        blogData: {
+          ...blogData,
+          formattedDate: formatDate(blogData.createdAt),
+        },
+        allBlogs,
+      },
+    };
+  } catch (err) {
+    return { redirect: { destination: "/not-found", permanent: false } };
+  }
 };
 
 const formatDate = (date) => {
