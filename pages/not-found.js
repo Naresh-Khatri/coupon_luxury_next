e3f22449cb52c21d44eed381c7166eb6b289471c
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-04 11:55:08 +0530

    create a 404 not found page

---
+++ pages/not-found.js
@@ -0,0 +1,67 @@
+import { Box, Button, Center, Flex, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import SetMeta from "../utils/SetMeta";
+
+function NotFoundPage() {
+  return (
+    <>
+      <SetMeta
+        title="404 CouponLuxury"
+        description="The page you are looking for does not exist."
+        url="https://www.couponluxury.com/not-found"
+      />
+      <Text as={'h1'} hidden>404 CouponLuxury</Text>
+      <Flex
+        w={"100vw"}
+        h={"100vh"}
+        direction="column"
+        justify="center"
+        align={"center"}
+      >
+        <Text fontSize={200} mt={-150} color="brand.900">
+          404
+        </Text>
+        <Text fontSize={20} mt={-10} mb={10} color="brand.900">
+          Oh no! The page is not found.
+        </Text>
+        <Image src={"/assets/404.svg"} width={300} height={300} alt={"404"} />
+        <Link href={"/"}>
+          <a>
+            <Button
+              mt={5}
+              bg="brand.900"
+              color="white"
+              shadow="0px 10px 33px -3px rgba(42, 129, 251, 0.5);"
+              _hover={{
+                bg: "brand.800",
+                shadow: "0px 10px 33px -3px rgba(42, 129, 251, 0.8)",
+              }}
+              w={"136"}
+              h={"50"}
+              fontSize={20}
+              px={5}
+              mb={5}
+            >
+              GO HOME
+            </Button>
+          </a>
+        </Link>
+      </Flex>
+    </>
+  );
+}
+
+// export const getStaticProps = async (ctx) => {
+//     if(ctx)
+//     console.log(ctx)
+//     return {
+//         redirect: {
+//             // destination: '/not-found',
+//             // permanent: false,
+//         },
+//     }
+// }
+export default NotFoundPage;
