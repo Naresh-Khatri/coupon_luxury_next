9053f317ce1c9f144ab2233eb648d68ed6eb237a
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-05 13:16:42 +0530

    create domain env for all api requests

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["ik.imagekit.io"],
   },
+  env: {
+    domain: 'http://localhost:4000',
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+// module.exports = withBundleAnalyzer({
+//   reactStrictMode: true,
+// });
