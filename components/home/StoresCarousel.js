fbd18161feb2e0cd29ab270c113ce8b75ef672e1
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-09 23:26:18 +0530

    add hover effect and increase size

--- components/home/StoresCarousel.js
+++ components/home/StoresCarousel.js
@@ -1,17 +1,11 @@
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Grid,
-  SimpleGrid,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import Image from "next/future/image";
+import { useState } from "react";
 
 function DealsOfTheDay({ featuredStores }) {
-  const options = {
+  const [isHover, setIsHover] = useState(false);
+  const initOptions = {
     type: "loop",
     autoplay: true,
     interval: 3000,
@@ -22,8 +16,9 @@ function DealsOfTheDay({ featuredStores }) {
     arrows: false,
 
     breakpoints: {
-      1024: { perPage: 8 },
-      768: { perPage: 6 },
+      1600: { perPage: 8 },
+      1024: { perPage: 7 },
+      768: { perPage: 5 },
       600: { perPage: 4 },
       500: { perPage: 3 },
     },
@@ -31,11 +26,18 @@ function DealsOfTheDay({ featuredStores }) {
     rewind: true,
     easing: "cubic-bezier(0.25, 1, 0.5, 1)",
   };
+  const [options, setOptions] = useState(initOptions);
 
   return (
     <Box w="full">
-      <Center p={10}>
-        <Text as={"h2"} fontSize="5xl" textAlign={"center"}>
+      {/* {isHover ? "true" : "false"} */}
+      <Center p={{ base: 5, md: 10 }} flexDir="column">
+        <Text
+          as={"h2"}
+          fontSize={{ base: "3xl", md: "5xl" }}
+          textAlign={"center"}
+          mb={5}
+        >
           More Than
           <Text
             as={"span"}
@@ -47,25 +49,35 @@ function DealsOfTheDay({ featuredStores }) {
           </Text>
           In One Place!
         </Text>
-      </Center>
-      <Center>
-        <Text as={"p"}  color={"gray.800"} >
+        <Text
+          as={"p"}
+          color={"gray.800"}
+          textAlign="center"
+          display={{ base: "none", md: "block" }}
+        >
           Search your favourite store & get many deals
         </Text>
       </Center>
-      <Flex h={160} my={10} direction="column" justifyContent="space-between">
-        <Splide
-          aria-label="Offers"
-          options={options}
-          className="carousel-container"
-        >
+      <Flex h={160} mb={5} direction="column" justifyContent="space-between">
+        <Splide aria-label="Offers" options={options}>
           {featuredStores.map((store) => (
             <SplideSlide key={store.id}>
-              <Box w={"100px"} h={"50px"} mt={5}>
+              <Box
+                w={{ base: "90px", md: "100px", lg: "120px" }}
+                h={{ base: "45px", md: "50px", lg: "60px" }}
+                mt={5}
+                _hover={{
+                  transform: "scale(1.1)",
+                  transition: "transform 0.2s ease-in",
+                }}
+                style={{
+                  transition: "transform 0.2s ease-in",
+                }}
+              >
                 <Image
                   style={{ borderRadius: "5px" }}
-                  width={80}
-                  height={40}
+                  width={120}
+                  height={60}
                   src={store.image}
                   alt={`${store.storeName} logo`}
                 />
@@ -73,19 +85,26 @@ function DealsOfTheDay({ featuredStores }) {
             </SplideSlide>
           ))}
         </Splide>
-        <Splide
-          aria-label="Offers"
-          options={{ ...options, direction: "rtl" }}
-          className="carousel-container"
-        >
+        <Splide aria-label="Offers" options={{ ...options, direction: "rtl" }}>
           {featuredStores.map((store) => (
             <SplideSlide key={store.id}>
-              <Box w={"100px"} h={"50px"} mt={5}>
+              <Box
+                w={{ base: "90px", md: "100px", lg: "120px" }}
+                h={{ base: "45px", md: "50px", lg: "60px" }}
+                mt={5}
+                _hover={{
+                  transform: "scale(1.1)",
+                  transition: "transform 0.2s ease-in",
+                }}
+                style={{
+                  transition: "transform 0.2s ease-in",
+                }}
+              >
                 <Image
                   style={{ borderRadius: "5px" }}
                   sizes={"100%"}
-                  width={80}
-                  height={40}
+                  width={120}
+                  height={60}
                   src={store.image}
                   alt={`${store.storeName} logo`}
                 />
