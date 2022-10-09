6b354b05eccae9117871164aaa327632aee81ed3
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-09 21:06:48 +0530

    add gradient to text

--- components/home/DealsOfTheDay.js
+++ components/home/DealsOfTheDay.js
@@ -8,6 +8,21 @@ function DealsOfTheDay({ deals }) {
   return (
     <Box as="section" mt={10}>
       <Text
+        as={"h2"}
+        fontSize={{ base: "4xl", md: "5xl" }}
+        textAlign={"center"}
+        mb={5}
+      >
+        <Text
+          as={"span"}
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip={"text"}
+        >
+          Deals{" "}
+        </Text>
+        of the day
+      </Text>
+      {/* <Text
         textAlign="center"
         fontSize={{ base: "4xl", md: "5xl" }}
         lineHeight="3.125rem"
@@ -16,9 +31,13 @@ function DealsOfTheDay({ deals }) {
         mb={5}
       >
         Deals of the day
-      </Text>
-      <Center>
-        <SimpleGrid columns={[2, 3, 5]} spacing={5} justifyContent="center">
+      </Text> */}
+      <Center mx={2}>
+        <SimpleGrid
+          columns={[2, 3,, 4, 5]}
+          spacing={{ base: 2, md: 5 }}
+          justifyContent="center"
+        >
           {deals.map((deal) => (
             <DealCard
               key={deal.id}
