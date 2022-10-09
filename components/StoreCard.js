926f9c49b5e704bce3d217a73ac36eab3d67eeb7
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-09 23:25:22 +0530

    add hover and increase height

--- components/StoreCard.js
+++ components/StoreCard.js
@@ -1,23 +1,45 @@
-import { Box, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
 import Image from "next/image";
 
 function StoreCard({ title, slug, img }) {
   return (
-    <Link href={`/stores/${slug}`}>
-      <Box shadow="2xl" borderRadius={10} bg="white">
-        <Image
-          src={img}
-          width={135}
-          height={65}
-          alt={`${title} - logo`}
-          sizes="100%"
-          style={{ borderRadius: "15px" }}
-        />
-        <Text py={2} textAlign={"center"} noOfLines={1}>
-          {title}
-        </Text>
-      </Box>
-    </Link>
+    <Box
+      style={{
+        transition: "transform 0.1s ease-in",
+      }}
+      _hover={{
+        transform: "scale(1.05)",
+        transition: "transform 0.1s ease-in",
+      }}
+      h="150px"
+    >
+      <Link href={`/stores/${slug}`}>
+        <Box
+          shadow="2xl"
+          borderRadius={10}
+          bg="white"
+          h={"full"}
+          display="flex"
+          flexDirection="column"
+        >
+          <Box w={"full"}>
+            <Image
+              src={img}
+              width={135}
+              height={65}
+              alt={`${title} - logo`}
+              sizes="100%"
+              style={{ borderRadius: "10px" }}
+            />
+          </Box>
+          <Flex justify={"center"} align="center" h={"full"}>
+            <Text p={2} textAlign={"center"} noOfLines={2}>
+              {title}
+            </Text>
+          </Flex>
+        </Box>
+      </Link>
+    </Box>
   );
 }
 
