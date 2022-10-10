e39a095f47bc094bd9a353b193de1d4d1ffca842
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-10 22:15:31 +0530

    use Link from next/link

--- components/StoreCard.js
+++ components/StoreCard.js
@@ -1,5 +1,6 @@
-import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 function StoreCard({ title, slug, img }) {
   return (
