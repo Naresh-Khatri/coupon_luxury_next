34f1e181ff1691d5e3091d1ed92a82dcbefffda3
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-10 17:56:49 +0530

    add an image loader for imagekit

---
+++ utils/imageKitLoader.js
@@ -0,0 +1,9 @@
+export const imageKitLoader = ({ src, width, quality }) => {
+  if (src[0] === "/") src = src.slice(1);
+  const params = [`w-${width}`];
+  if (quality) {
+    params.push(`q-${quality}`);
+  }
+  const paramsString = params.join(",");
+  return `${src}?tr=${paramsString}`;
+};
