30fef2b35287c563ab727ed68d33352d385115de
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-16 19:07:35 +0530

    make text semibold

--- components/RecommendedStores.js
+++ components/RecommendedStores.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function RecommendedStores({ stores }) {
   return (
-    <Box p={4} bg="white" borderRadius={15}>
+    <Box p={4} bg="white" borderRadius={15} fontWeight={"semibold"}>
       <Text color="black" fontSize={"2xl"}>
         Popular Stores
       </Text>
@@ -33,6 +33,150 @@ function RecommendedStores({ stores }) {
             </a>
           </Link>
         ))}
+        {/* {stores.map((store) => (
+          <Link key={store.id} href={`/stores/${store.slug}`}>
+            <a>
+              <Flex
+                justify={"center"}
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "transform 0.1s ease-in-out",
+                }}
+                style={{
+                  transition: "transform 0.1s ease-in-out",
+                }}
+              >
+                <Image
+                  src={store.image}
+                  alt={`${store.storeName} - logo`}
+                  width={100}
+                  height={50}
+                  style={{ borderRadius: "5px" }}
+                />
+              </Flex>
+            </a>
+          </Link>
+        ))}
+        {stores.map((store) => (
+          <Link key={store.id} href={`/stores/${store.slug}`}>
+            <a>
+              <Flex
+                justify={"center"}
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "transform 0.1s ease-in-out",
+                }}
+                style={{
+                  transition: "transform 0.1s ease-in-out",
+                }}
+              >
+                <Image
+                  src={store.image}
+                  alt={`${store.storeName} - logo`}
+                  width={100}
+                  height={50}
+                  style={{ borderRadius: "5px" }}
+                />
+              </Flex>
+            </a>
+          </Link>
+        ))}
+        {stores.map((store) => (
+          <Link key={store.id} href={`/stores/${store.slug}`}>
+            <a>
+              <Flex
+                justify={"center"}
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "transform 0.1s ease-in-out",
+                }}
+                style={{
+                  transition: "transform 0.1s ease-in-out",
+                }}
+              >
+                <Image
+                  src={store.image}
+                  alt={`${store.storeName} - logo`}
+                  width={100}
+                  height={50}
+                  style={{ borderRadius: "5px" }}
+                />
+              </Flex>
+            </a>
+          </Link>
+        ))}
+        {stores.map((store) => (
+          <Link key={store.id} href={`/stores/${store.slug}`}>
+            <a>
+              <Flex
+                justify={"center"}
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "transform 0.1s ease-in-out",
+                }}
+                style={{
+                  transition: "transform 0.1s ease-in-out",
+                }}
+              >
+                <Image
+                  src={store.image}
+                  alt={`${store.storeName} - logo`}
+                  width={100}
+                  height={50}
+                  style={{ borderRadius: "5px" }}
+                />
+              </Flex>
+            </a>
+          </Link>
+        ))}
+        {stores.map((store) => (
+          <Link key={store.id} href={`/stores/${store.slug}`}>
+            <a>
+              <Flex
+                justify={"center"}
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "transform 0.1s ease-in-out",
+                }}
+                style={{
+                  transition: "transform 0.1s ease-in-out",
+                }}
+              >
+                <Image
+                  src={store.image}
+                  alt={`${store.storeName} - logo`}
+                  width={100}
+                  height={50}
+                  style={{ borderRadius: "5px" }}
+                />
+              </Flex>
+            </a>
+          </Link>
+        ))}
+        {stores.map((store) => (
+          <Link key={store.id} href={`/stores/${store.slug}`}>
+            <a>
+              <Flex
+                justify={"center"}
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "transform 0.1s ease-in-out",
+                }}
+                style={{
+                  transition: "transform 0.1s ease-in-out",
+                }}
+              >
+                <Image
+                  src={store.image}
+                  alt={`${store.storeName} - logo`}
+                  width={100}
+                  height={50}
+                  style={{ borderRadius: "5px" }}
+                />
+              </Flex>
+            </a>
+          </Link>
+        ))} */}
       </SimpleGrid>
       <Center>
         <Link href={"/stores"}>
