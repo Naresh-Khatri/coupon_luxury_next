cfa0e1bf6a47214fbf6b2ff1dea3675461ea14ca
naresh-khatri <naresh.khatri2345@gmail.com>
2023-03-08 00:08:52 +0530

    remove url rewrite from /about to /

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-/** @type {import('next').NextConfig} */
-
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-// });
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,20 +5,8 @@ const nextConfig = {
     domains: ["ik.imagekit.io"],
   },
   env: {
-    // domain: 'http://localhost:4000',
     domain: "https://apiv2.couponluxury.com",
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/about",
-        destination: "/",
-      },
-    ];
   },
 };
 
 module.exports = nextConfig;
-// module.exports = withBundleAnalyzer({
-//   reactStrictMode: true,
-// });
