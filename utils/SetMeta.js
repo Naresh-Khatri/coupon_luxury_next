469be4a30df644fe8277368ac2443596b5d019c0
naresh-khatri <naresh.khatri2345@gmail.com>
2023-03-10 16:51:12 +0530

    refactor SetMeta to include default values if not passed

--- utils/SetMeta.js
+++ utils/SetMeta.js
@@ -2,42 +2,50 @@ import Head from "next/head";
 import React from "react";
 
 function SetMeta({ title, description, keywords, image, url }) {
+  title = title
+    ? title
+    : "Couponluxury: Deals, coupon codes, Discounts & offers";
+  description = description
+    ? description
+    : `Avail the most luxurious deals and promo codes to get the best discount offers while shopping from brands. Exclusive coupon codes available on CouponLuxury`;
+  keywords = keywords
+    ? keywords
+    : `coupons, coupon codes, promo codes, discount codes, deals, online shopping, offers, vouchers, cashbacks`;
+  image = image
+    ? image
+    : "https://ik.imagekit.io/couponluxury/zyro-image__3__2Dw77Pooe.png";
+  url = url ? url : "https://www.couponluxury.com/";
+
   return (
     <Head>
-      {title && (
-        <>
-          <title>{title}</title>
-          <meta property="og:title" content={title} />
-          <meta name="twitter:title" content={title} />
-        </>
-      )}
-      {description && (
-        <>
-          <meta name="description" content={description} />
-          <meta property="og:description" content={description} />
-          <meta name="twitter:description" content={description} />
-        </>
-      )}
-      {keywords && (
-        <>
-          <meta name="keywords" content={keywords} />
-        </>
-      )}
-      {image && (
-        <>
-          <meta property="og:image" content={image} />
-          <meta name="twitter:image" content={image} />
-        </>
-      )}
-      {url && (
-        <>
-          <meta name="url" content={url.toLowerCase()} />
-          <meta property="og:url" content={url.toLowerCase()} />
-          <meta name="twitter:url" content={url.toLowerCase()} />
-          <link rel="canonical" href={url.toLowerCase()} />
-        </>
-      )}
-      {!url && <link rel="canonical" href="https://www.couponluxury.com" />}
+      {/* TITLE */}
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta name="twitter:title" content={title} />
+
+      {/* DESCRIPTION */}
+      <meta name="description" content={description} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:description" content={description} />
+
+      {/* KEYWORDS */}
+      <meta name="keywords" content={keywords} />
+
+      {/* IMAGES */}
+      <meta property="og:image" content={image} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={image} />
+      <meta
+        name="twitter:image:alt"
+        content="CouponLuxury.com Coupon Codes and Discounts"
+      />
+      <meta itemProp="image" content={image} />
+
+      {/* URL */}
+      <meta name="url" content={url.toLowerCase()} />
+      <meta property="og:url" content={url.toLowerCase()} />
+      <meta name="twitter:url" content={url.toLowerCase()} />
+      <link rel="canonical" href={url.toLowerCase()} />
     </Head>
   );
 }
