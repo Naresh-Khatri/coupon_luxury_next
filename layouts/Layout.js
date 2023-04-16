723e8551ad149eda5a20026d6520b3e8a86cfd82
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-16 16:55:12 +0530

    hide layout on specified pathnames

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -3,15 +3,26 @@ import React from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import LoadingOverlay from "../components/LoadingOverlay";
+import { useRouter } from "next/router";
+const hideLayouts = ["/blogs/admin"];
+
 function Layout({ children }) {
+  const router = useRouter();
+
   return (
     <>
       <LoadingOverlay>
-        <Box position={"fixed"} w={"100%"} zIndex={10} top={0}>
-          <NavBar />
-        </Box>
-        <Box pt={{ base: "59px", lg: "75px" }}>{children}</Box>
-        <Footer />
+        {hideLayouts.includes(router.pathname) ? (
+          children
+        ) : (
+          <>
+            <Box position={"fixed"} w={"100%"} zIndex={10} top={0}>
+              <NavBar />
+            </Box>
+            <Box pt={{ base: "59px", lg: "75px" }}>{children}</Box>
+            <Footer />
+          </>
+        )}
       </LoadingOverlay>
     </>
   );
