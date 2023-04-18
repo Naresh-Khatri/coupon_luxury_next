9934406b3573962cd01fbae3bff5a361f6393654
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-18 16:55:12 +0530

    add validation and extract away components

--- pages/blogs/admin.js
+++ pages/blogs/admin.js
@@ -1,16 +1,12 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import account from "../../appwrite/config";
 import {
   Box,
   Button,
   Container,
   Flex,
-  FormControl,
-  FormLabel,
   HStack,
-  Input,
   SimpleGrid,
-  Stack,
   Tab,
   TabList,
   TabPanel,
@@ -22,19 +18,57 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { AddIcon, EditIcon, HamburgerIcon } from "@chakra-ui/icons";
+import { AddIcon, HamburgerIcon } from "@chakra-ui/icons";
 import CustomEditor from "../../components/CustomEditor";
-import { useDropzone } from "react-dropzone";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImage } from "@fortawesome/free-regular-svg-icons";
-import { faDownLong } from "@fortawesome/free-solid-svg-icons";
 
-export default function SimpleCard() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNewspaper } from "@fortawesome/free-regular-svg-icons";
+import { convertCanvasToBlob } from "../../lib/lib";
+import axios from "axios";
+import LeftForm from "../../components/BlogPanel/LeftForm";
+import ImageCropper from "../../components/BlogPanel/ImageCropper";
+
+export default function BlogAdminPage() {
   const router = useRouter();
   const toast = useToast();
 
   const [userInfo, setUserInfo] = useState({});
   const [userIsLoading, setUserIsLoading] = useState(true);
+
+  const [title, setTitle] = useState("");
+  const [slug, setSlug] = useState("");
+  const [imageAlt, setImageAlt] = useState("");
+  const [metaTitle, setMetaTitle] = useState("");
+  const [metaDescription, setMetaDescription] = useState("");
+  const [metaSchema, setMetaSchema] = useState(`{
+  "@context": "https://schema.org",
+  "@type": "BlogPosting",
+  "headline": "Example Blog Post Title",
+  "datePublished": "2023-05-30",
+  "author": {
+    "@type": "Person",
+    "name": "John Doe"
+  },
+  "image": "https://example.com/image.jpg",
+  "description": "This is an example blog post.",
+  "publisher": {
+    "@type": "Organization",
+    "name": "Example Blog",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://example.com/logo.jpg",
+      "width": 600,
+      "height": 60
+    }
+  }
+}`);
+  const [isPublising, setIsPublishing] = useState(false);
+
+  const tinymceEditorRef = useRef(null);
+
+  const cropperRef1 = useRef(null);
+  const cropperRef2 = useRef(null);
+  const [coverImg, setCoverImg] = useState(null);
 
   useEffect(() => {
     setUserIsLoading(true);
@@ -42,6 +76,7 @@ export default function SimpleCard() {
       .get()
       .then((res) => {
         setUserInfo(res);
+        console.log(res);
         setUserIsLoading(false);
       })
       .catch((err) => {
@@ -58,21 +93,127 @@ export default function SimpleCard() {
         });
       });
   }, [router, toast]);
-  const handleLogout = () => {
-    account
-      .deleteSession("current")
-      .then(() => {
-        setUserInfo({});
-      })
-      .catch((err) => {
-        console.log(err);
+
+  const onFileDrop = (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setCoverImg(e.target.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handlePublish = async () => {
+    setIsPublishing(true);
+    const titleIsValid = title.trim().length >= 20;
+    const slugIsValid = slug.trim().length >= 20;
+    const imageAltIsValid = imageAlt.trim().length >= 20;
+    const contentIsValid =
+      tinymceEditorRef.current.getContent().trim().length >= 20;
+    const coverImgIsValid = coverImg !== null;
+    const metaTitleIsValid =
+      metaTitle.trim().length >= 30 && metaTitle.trim().length <= 70;
+    const metaDescriptionIsValid =
+      metaDescription.trim().length >= 50 &&
+      metaDescription.trim().length <= 160;
+    const metaSchemaIsValid = metaSchema.length > 30;
+    const formIsValid =
+      titleIsValid &&
+      slugIsValid &&
+      imageAltIsValid &&
+      contentIsValid &&
+      coverImgIsValid &&
+      metaTitleIsValid &&
+      metaDescriptionIsValid &&
+      metaSchemaIsValid;
+    if (!formIsValid) {
+      toast({
+        title: `${
+          !titleIsValid
+            ? "Title"
+            : !slugIsValid
+            ? "Slug"
+            : !imageAltIsValid
+            ? "Image Alt"
+            : !contentIsValid
+            ? "Content"
+            : !coverImgIsValid
+            ? "Cover Image"
+            : !metaTitleIsValid
+            ? "Meta Title"
+            : !metaDescriptionIsValid
+            ? "Meta Description"
+            : !metaSchemaIsValid
+            ? "Meta Schema"
+            : ""
+        } is not valid`,
+        description: "Please check the form",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
       });
+      setIsPublishing(false);
+      return;
+    }
+
+    const coverImgCropped = await convertCanvasToBlob(
+      cropperRef1.current.getCanvas()
+    );
+    const thumnailImgCropped = await convertCanvasToBlob(
+      cropperRef2.current.getCanvas()
+    );
+    const { jwt } = await account.createJWT("current");
+    const formData = new FormData();
+
+    formData.append("coverImg", coverImgCropped);
+    formData.append("thumbnailImg", thumnailImgCropped);
+    formData.append("title", title);
+    formData.append("smallDescription", metaDescription);
+    formData.append("fullDescription", tinymceEditorRef.current.getContent());
+    formData.append("slug", slug);
+    formData.append("imgAlt", imageAlt);
+    formData.append("metaTitle", metaTitle);
+    formData.append("metaDescription", metaDescription);
+    formData.append("metaSchema", metaSchema);
+    try {
+      const res = await axios.post(
+        // "https://apiv2.couponluxury.com/blogs",
+        "http://localhost:4000/blogs/v2/",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      );
+      setIsPublishing(false);
+      toast({
+        title: "Success",
+        description: "Your post has been published",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      router.push("/blogs/" + res.data.slug);
+      await account.deleteSession("current");
+      console.log(res);
+    } catch (err) {
+      setIsPublishing(false);
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      throw err;
+    }
   };
 
   if (userIsLoading) return <div>Loading...</div>;
   return (
     <>
-      <NavBar handleLogout={handleLogout} />
+      <NavBar />
       <Container maxW="container.xl">
         <Tabs defaultIndex={1}>
           <TabList>
@@ -87,13 +228,61 @@ export default function SimpleCard() {
               </HStack>
             </Tab>
           </TabList>
-
           <TabPanels>
             <TabPanel>
               <ListPosts posts={[]} />
             </TabPanel>
             <TabPanel>
-              <CreateBlog />
+              <Container
+                maxW="container.xl"
+                shadow={"md"}
+                bg={"#f9f9f9"}
+                borderRadius={"lg"}
+              >
+                <SimpleGrid columns={2} spacing={10}>
+                  <Box>
+                    <LeftForm
+                      title={title}
+                      setTitle={setTitle}
+                      slug={slug}
+                      setSlug={setSlug}
+                      imageAlt={imageAlt}
+                      setImageAlt={setImageAlt}
+                      metaTitle={metaTitle}
+                      setMetaTitle={setMetaTitle}
+                      metaDescription={metaDescription}
+                      setMetaDescription={setMetaDescription}
+                      metaSchema={metaSchema}
+                      setMetaSchema={setMetaSchema}
+                    />
+                  </Box>
+                  <Box p={5}>
+                    <ImageCropper
+                      coverImg={coverImg}
+                      setCoverImg={setCoverImg}
+                      coverImgRef={cropperRef1}
+                      thumbnailImgRef={cropperRef2}
+                      onFileDrop={onFileDrop}
+                    />
+                  </Box>
+                </SimpleGrid>
+                <CustomEditor editorRef={tinymceEditorRef} />
+              </Container>
+              <Flex justifyContent={"end"}>
+                <Button colorScheme="red" disabled size="lg" m={5}>
+                  Cancel
+                </Button>
+                <Button
+                  leftIcon={<FontAwesomeIcon icon={faNewspaper} />}
+                  colorScheme="teal"
+                  size="lg"
+                  m={5}
+                  onClick={handlePublish}
+                  isLoading={isPublising}
+                >
+                  Publish
+                </Button>
+              </Flex>
             </TabPanel>
           </TabPanels>
         </Tabs>
@@ -102,67 +291,17 @@ export default function SimpleCard() {
   );
 }
 
-const CreateBlog = () => {
-  return (
-    <Container maxW="container.xl" shadow={"md"} borderRadius={"lg"}>
-      <SimpleGrid columns={2} spacing={10}>
-        <Box p={5}>
-          <Stack spacing={3}>
-            <FormControl isRequired>
-              <FormLabel>Blog Title</FormLabel>
-              <Input placeholder="Enter Title" />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Blog Description</FormLabel>
-              <Input placeholder="Enter Title" />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Blog Slug</FormLabel>
-              <Input placeholder="Enter Slug" />
-            </FormControl>
-          </Stack>
-        </Box>
-        <Box p={5}>
-          <MyDropzone />
-        </Box>
-      </SimpleGrid>
-      <CustomEditor />
-    </Container>
-  );
-};
-const MyDropzone = () => {
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log(acceptedFiles);
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <Flex
-        justifyContent={"center"}
-        alignItems={"center"}
-        borderRadius={10}
-        border={`2px dashed ${isDragActive ? "lightgreen" : "gray"}`}
-        h={200}
-      >
-        {!isDragActive ? (
-          <Stack>
-            <FontAwesomeIcon icon={faImage} bounce size="5x" />
-            <Text color={"gray.400"}>Drop the files here...</Text>
-          </Stack>
-        ) : (
-          <Stack>
-            <FontAwesomeIcon icon={faDownLong} color="green" bounce size="5x" />
-            <Text color={"green.400"}>Drop it like it's hot!</Text>
-          </Stack>
-        )}
-      </Flex>
-    </div>
-  );
-};
-
-const NavBar = ({ handleLogout }) => {
+const NavBar = () => {
+  const handleLogout = () => {
+    account
+      .deleteSession("current")
+      .then(() => {
+        setUserInfo({});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <Flex
@@ -192,7 +331,6 @@ const NavBar = ({ handleLogout }) => {
 };
 
 const ListPosts = ({ posts }) => {
-  console.log(posts);
   if (!posts || posts.length === 0) return <Text>No posts</Text>;
   return (
     <Box>
