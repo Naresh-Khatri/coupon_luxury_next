ac11296ebe4f0982a57a9471e8e28889d3710333
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-18 16:55:12 +0530

    create tinymce editor component

---
+++ components/CustomEditor.js
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Box } from "@chakra-ui/react";
+
+const CustomEditor = () => {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  return (
+    <Box py={3}>
+      <Editor
+        apiKey="j3b4s2ajya2zgs4whxrd52dy6239k4mblodx2wn6j8vr6ptv"
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          selector: "textarea",
+          statusbar: false,
+          plugins:
+            "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
+          toolbar:
+            "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
+        }}
+      />
+    </Box>
+  );
+};
+
+export default CustomEditor;
