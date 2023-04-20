a7f6dce71b43dfb7f5727e3ca358a7ef98713e8c
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-20 16:55:12 +0530

    add shadows and remove unused log function

--- components/CustomEditor.js
+++ components/CustomEditor.js
@@ -3,28 +3,24 @@ import { Editor } from "@tinymce/tinymce-react";
 import { Box, Text } from "@chakra-ui/react";
 
 const CustomEditor = ({ editorRef }) => {
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
-
   return (
     <Box py={3}>
       <Text>Content: </Text>
-      <Editor
-        apiKey="j3b4s2ajya2zgs4whxrd52dy6239k4mblodx2wn6j8vr6ptv"
-        onInit={(evt, editor) => (editorRef.current = editor)}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          selector: "textarea",
-          statusbar: false,
-          plugins:
-            "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
-          toolbar:
-            "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
-        }}
-      />
+      <Box shadow={"md"} borderRadius={10}>
+        <Editor
+          apiKey="j3b4s2ajya2zgs4whxrd52dy6239k4mblodx2wn6j8vr6ptv"
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          initialValue="<p>This is the initial content of the editor.</p>"
+          init={{
+            selector: "textarea",
+            statusbar: false,
+            plugins:
+              "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
+            toolbar:
+              "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
+          }}
+        />
+      </Box>
     </Box>
   );
 };
