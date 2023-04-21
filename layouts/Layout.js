eaad7a91fe5a9c99125d31abea43427efced0c2b
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-21 16:55:12 +0530

    register admin layout for admin routes

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,9 +1,9 @@
 import { SkipNavLink, Box } from "@chakra-ui/react";
 import React from "react";
-import Footer from "../components/Footer";
-import NavBar from "../components/NavBar";
 import LoadingOverlay from "../components/LoadingOverlay";
 import { useRouter } from "next/router";
+import NormalLayout from "./NormalLayout";
+import AdminLayout from "./AdminLayout";
 const layoutHiddenRoutes = ["/blogs/admin"];
 
 function Layout({ children }) {
@@ -13,15 +13,9 @@ function Layout({ children }) {
     <>
       <LoadingOverlay>
         {layoutHiddenRoutes.some((route) => router.pathname.includes(route)) ? (
-          children
+          <AdminLayout>{children}</AdminLayout>
         ) : (
-          <>
-            <Box position={"fixed"} w={"100%"} zIndex={10} top={0}>
-              <NavBar />
-            </Box>
-            <Box pt={{ base: "59px", lg: "75px" }}>{children}</Box>
-            <Footer />
-          </>
+          <NormalLayout>{children}</NormalLayout>
         )}
       </LoadingOverlay>
     </>
