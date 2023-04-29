e65fea464d0113dde97c6ea03e9883b65f7983ca
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-29 16:55:12 +0530

    feat: new prop to show errors as users types and prevent typing more than allowed

--- components/RuledInput.js
+++ components/RuledInput.js
@@ -4,37 +4,67 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 
-function RuledInput({ title, placeholder, hintText, value, setValue, rule }) {
+function RuledInput({
+  title,
+  placeholder,
+  hintText,
+  value,
+  setValue,
+  rule,
+  earlyShowError,
+}) {
+  const toast = useToast();
   const [inputTouched, setInputTouched] = useState(false);
-  const inputIsInvalid =
-    inputTouched &&
-    (value.trim().length < rule.min || value.trim().length > rule.max);
+  let inputIsInvalid = null;
+  if (earlyShowError) {
+    inputIsInvalid =
+      value.trim().length > 0 &&
+      (value.trim().length < rule.min || value.trim().length > rule.max);
+  } else {
+    inputIsInvalid =
+      inputTouched &&
+      (value.trim().length < rule.min || value.trim().length > rule.max);
+  }
+
+  const handleOnChange = (e) => {
+    if (e.target.value.length > rule.max) {
+      toast({
+        title: "Input too long",
+        description: `Length: ${e.target.value.length}, Maximum allowed ${rule.max}`,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+
+      return;
+    }
+    setValue(e.target.value);
+  };
   return (
-    <FormControl isRequired isInvalid={inputIsInvalid && inputTouched}>
+    <FormControl isRequired isInvalid={inputIsInvalid}>
       <FormLabel>{title}</FormLabel>
       <Input
         placeholder={placeholder}
         value={value}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={handleOnChange}
         onBlur={() => setInputTouched(true)}
       />
-      {inputIsInvalid ? 
-        inputTouched &&(
-        <FormErrorMessage fontSize={'12.5'}>
-          {value.trim().length < 30 &&
-            `Length: ${value.trim().length}, Minimum allowed ${rule.min}`}
-          {value.trim().length > 70 &&
-            `Length: ${value.trim().length}, Maximum allowed ${rule.max}`}
-        </FormErrorMessage>
-        
-      ) : (
-        <FormHelperText fontSize={'12.5'}>
-          Length: {value.trim().length} ({hintText})
-        </FormHelperText>
-      )}
+      {/* {inputIsInvalid ? ( */}
+      <FormErrorMessage fontSize={"12.5"}>
+        {value.trim().length < 30 &&
+          `Length: ${value.trim().length}, Minimum allowed ${rule.min}`}
+        {value.trim().length > 70 &&
+          `Length: ${value.trim().length}, Maximum allowed ${rule.max}`}
+      </FormErrorMessage>
+      {/* ) : ( */}
+      <FormHelperText fontSize={"12.5"}>
+        Length: {value.trim().length} ({hintText})
+      </FormHelperText>
+      {/* )} */}
     </FormControl>
   );
 }
