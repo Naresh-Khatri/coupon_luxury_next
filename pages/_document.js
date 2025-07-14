1816f1524a0cbd4fa8cf7712b43c83d418ce94db
Naresh Khatri <naresh.khatri2345@gmail.com>
2025-07-14 09:38:40 +0530

    feat: add new script

--- pages/_document.js
+++ pages/_document.js
@@ -66,6 +66,11 @@ export default function Document() {
             `,
           }}
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2253863056028852"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
